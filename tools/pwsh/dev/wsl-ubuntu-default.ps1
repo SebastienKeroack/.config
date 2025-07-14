f97e95fa2e69161e13c2a863500c405c1a687382
Sébastien Kéroack <dev@sebastienkeroack.com>
2025-07-14 13:26:43 -0400

    fix(wsl): correct apt-get upgrade command syntax

--- tools/pwsh/dev/wsl-ubuntu-default.ps1
+++ tools/pwsh/dev/wsl-ubuntu-default.ps1
@@ -36,16 +36,21 @@ You may be prompted for your password.
 "@
   wsl -d $WSLDist -e sudo bash -c @'
 apt-get update
-apt-get upgrade -&
+apt-get upgrade -y
+
 # Install required packages for asdf and Python
 apt-get install -y bash git unzip zip
+
 # Install required packages to compile TensorFlow
 apt-get install -y make llvm-18 clang-18 clang-format
+
 # Set clang and clang++ to use version 18 by default
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-18 100
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-18 100
+
 # Set LLVM to use version 18 by default
 update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-18 100
+
 # Install required packages to compile Python
 apt-get install -y build-essential gdb lcov pkg-config \
   libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
